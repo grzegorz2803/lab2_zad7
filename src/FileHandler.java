3bc1361db3c1750d5e3964fd844f84a9493d827c
Grzegorz Listwan <listwan94@gmail.com>
2023-11-19 18:21:50 +0100

    Add FileHandler class with witeLine() function

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,2 +1,22 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class FileHandler {
+    private String fileContent = "";
+    private Lock lock = new ReentrantLock();
+    private Condition authorCondition = lock.newCondition();
+    private Condition writerCondition = lock.newCondition();
+
+    private void writeLine(String line) throws InterruptedException{
+        lock.lock();
+        try{
+            writerCondition.await(); // autor czeka na odczyt poprzedniej lini
+            fileContent +=line +"\n";
+            System.out.println("Autor wpisał: " + line); // autor wpisuje kolejną linie
+            authorCondition.signal(); // informujemy że linia została dodana
+        }finally {
+            lock.unlock();
+        }
+    }
 }
