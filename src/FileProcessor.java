f114b5630ed73d4fa527765b36147aab197d11a9
Grzegorz Listwan <listwan94@gmail.com>
2023-11-20 10:54:27 +0100

    Add new class FileProcessor

---
+++ src/FileProcessor.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class FileProcessor {
+    private  String fileName;
+    private  String content;
+
+    public  FileProcessor(String fileName){
+        this.fileName = fileName;
+    }
+    public synchronized void readFromFIle() throws IOException, InterruptedException{
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine())!=null){
+                sb.append(line).append("\n");
+            }
+            content = sb.toString();
+            System.out.println("Autor: Odczytał zawartość pliku");
+        }
+    }
+    public synchronized void wiriteToFile() throws  IOException, InterruptedException {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))){
+            writer.write("Pisarz napisał nową linie. \n");
+            System.out.println("Pisarz wpisał nową linie");
+        }
+    }
+    public synchronized String getContent(){
+        return  content;
+    }
+}
